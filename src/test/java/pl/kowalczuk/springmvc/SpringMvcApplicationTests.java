1de6243769da042bba6c5dc96ecf0a2874b60294
czader98 <czader98@poczta.fm>
2020-05-03 11:50:36 +0200

    Initial commit

---
+++ src/test/java/pl/kowalczuk/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowalczuk.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
