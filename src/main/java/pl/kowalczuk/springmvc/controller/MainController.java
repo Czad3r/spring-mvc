421d955bd9d3bc22854ff532d10c618422658ad0
czader98 <czader98@poczta.fm>
2020-05-03 21:05:58 +0200

    Added source download button

--- src/main/java/pl/kowalczuk/springmvc/controller/MainController.java
+++ src/main/java/pl/kowalczuk/springmvc/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 import pl.kowalczuk.springmvc.repository.QuoteRepository;
 
 @Controller
@@ -12,10 +13,6 @@ public class MainController {
     @Autowired
     private QuoteRepository quoteRepository;
 
-    @GetMapping("/contact")
-    public String contact() {
-        return "contact";
-    }
 
     @GetMapping("/registerForm")
     public String registerForm() {
@@ -29,12 +26,21 @@ public class MainController {
 
     @GetMapping("/quotesList")
     public String quotesList(Model model) {
-        model.addAttribute("quotes",quoteRepository.findAll());
+        model.addAttribute("quotes", quoteRepository.findAll());
         return "quotesList";
     }
 
     @GetMapping("/memoryGame")
     public String memoryGame() {
         return "memoryGame";
+    }
+
+
+
+    @GetMapping("/downloadSource")
+    public RedirectView downloadSource() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("https://github.com/Czad3r/spring-mvc/archive/master.zip");
+        return redirectView;
     }
 }
