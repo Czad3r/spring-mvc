16a3617568c1c5ec685914eb1fbb5335d009638e
czader98 <czader98@poczta.fm>
2020-05-30 13:00:06 +0200

    + Language resolver fix

--- src/main/java/pl/kowalczuk/springmvc/config/LanguageConfig.java
+++ src/main/java/pl/kowalczuk/springmvc/config/LanguageConfig.java
@@ -3,8 +3,8 @@ package pl.kowalczuk.springmvc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -12,8 +12,9 @@ import java.util.Locale;
 public class LanguageConfig {
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
+        CookieLocaleResolver slr = new CookieLocaleResolver();
         slr.setDefaultLocale(Locale.forLanguageTag("pl"));
+        slr.setCookieName("lang");
         return slr;
     }
 
